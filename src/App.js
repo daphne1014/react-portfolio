a9c6c9eab8230f1641f2fa40fc6e53c88df843f2
Daphne Song <85268906+daphne1014@users.noreply.github.com>
2021-10-23 23:54:10 -0700

    added About & Project Section

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
-import React from "react";
-import Nav from "./components/Nav";
-import About from "./components/About";
-
+import './App.css';
+import './assets/css/Header.css'
+import './assets/css/About.css'
+import './assets/css/Home.css'
+import Header from './components/Header';
+import About from './components/About';
+import Home from './components/Home';
+import Projects from './components/Projects';
+import Footer from './components/Footer';
 
 function App() {
   return (
-    <div>
-      <Nav />
-      <main>
-        <About />
-      </main>
+    <div className="App">
+      <Header />
+      <Home />
+      <About />
+      <Projects />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Footer/index.js
@@ -0,0 +1,36 @@
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {faGoogle, faLinkedin, faGithub } from "@fortawesome/free-brands-svg-icons";
+
+
+function Footer() {
+    return (
+        <footer class="text-center text-lg-start bg-light text-mute">
+            <section class="d-flex justify-content-center p-4 border-bottom">
+                <div>
+                    <a href="mailto: daphnesong1014@gmail.com" class="me-4 text-reset">
+                    <FontAwesomeIcon
+                            icon={faGoogle}
+                            className="icon-size-1He icon-color"
+                        />
+                    </a>
+                    <a href="https://www.linkedin.com/in/daphne-song/" class="me-4 text-reset">
+                        <FontAwesomeIcon
+                            icon={faLinkedin}
+                            className="icon-size-1He icon-color"
+                        />
+                    </a>
+                    <a href="https://github.com/daphne1014" class="me-4 text-reset">
+                        <FontAwesomeIcon
+                            icon={faGithub}
+                            className="icon-size-1He icon-color"
+                        />
+
+                    </a>
+                </div>
+            </section>
+        </footer>
+    )
+}
+
+export default Footer
